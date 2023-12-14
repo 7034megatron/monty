b9c98f96d47bffbf12177d883e974675dbc8cd1e
7034megatron <emmanuelmabhena0@gmail.com>
2023-12-14 00:17:23 -0800

    updated betty

--- stack.c
+++ stack.c
@@ -1,78 +1,77 @@
 #include "monty.h"
 
-void push(stack_t **stack, int value) {
-    stack_t *newNode = malloc(sizeof(stack_t));
-    if (!newNode) {
-        fprintf(stderr, "Error: malloc failed\n");
-        exit(EXIT_FAILURE);
-    }
+void push(stack_t **stack, int value)
+{
+	stack_t *newNode = malloc(sizeof(stack_t));
 
-    newNode->n = value;
-    newNode->prev = NULL;
-    newNode->next = *stack;
-
-    if (*stack != NULL) {
-        (*stack)->prev = newNode;
-    }
-
-    *stack = newNode;
+	if (!newNode)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	newNode->n = value;
+	newNode->prev = NULL;
+	newNode->next = *stack;
+	if (*stack != NULL)
+	{
+		(*stack)->prev = newNode;
+	}
+	*stack = newNode;
 }
+void pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp = *stack;
+	*stack = (*stack)->next;
 
-void pop(stack_t **stack, unsigned int line_number) {
-    stack_t *temp = *stack;
-    *stack = (*stack)->next;
-
-    if (*stack == NULL) {
-        fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-
-
-
-    if (*stack != NULL) {
-        (*stack)->prev = NULL;
-    }
-
-    free(temp);
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if (*stack != NULL)
+	{
+		(*stack)->prev = NULL;
+	}
+	free(temp);
 }
-
-void pall(stack_t **stack, unsigned int line_number) {
-    stack_t *current = *stack;
-    (void)line_number;
-    while (current != NULL) {
-        printf("%d\n", current->n);
-        current = current->next;
-    }
+void pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current = *stack;
+	(void)line_number;
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+	}
 }
-
-void pint(stack_t **stack, unsigned int line_number) {
-    if (*stack == NULL) {
-        fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-
-    printf("%d\n", (*stack)->n);
+void pint(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*stack)->n);
 }
+void swap(stack_t **stack, unsigned int line_number)
+{
+	int temp = (*stack)->n;
 
-void swap(stack_t **stack, unsigned int line_number) {
-    int temp = (*stack)->n;
-
-    if (*stack == NULL || (*stack)->next == NULL) {
-        fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-
-
-    (*stack)->n = (*stack)->next->n;
-    (*stack)->next->n = temp;
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	(*stack)->n = (*stack)->next->n;
+	(*stack)->next->n = temp;
 }
-
-void add(stack_t **stack, unsigned int line_number) {
-    if (*stack == NULL || (*stack)->next == NULL) {
-        fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-
-    (*stack)->next->n += (*stack)->n;
-    pop(stack, line_number);
+void add(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	(*stack)->next->n += (*stack)->n;
+	pop(stack, line_number);
 }
